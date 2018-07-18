1fe4471e5f674469eaa5653da11664605215673f
Aaron Lee <jjongaya83@gmail.com>
2018-07-18 14:12:26 +0900

    서버 호출 인터페이스에 사용자 조회 메소드와 새로운 사용자 전화번호 등록 메소드 구현

--- app/src/main/java/com/aaronlee/bestfood/remote/RemoteService.java
+++ app/src/main/java/com/aaronlee/bestfood/remote/RemoteService.java
@@ -1,5 +1,14 @@
 package com.aaronlee.bestfood.remote;
 
+import com.aaronlee.bestfood.item.MemberInfoItem;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
 /**
  * 서버에 호출할 메소드를 선언하는 인터페이스
  */
@@ -8,4 +17,12 @@ public interface RemoteService {
     String MEMBER_ICON_URL = BASE_URL + "/member/";
     String IMAGE_URL = BASE_URL + "/img/";
 
+    // 사용자 정보 조회
+    @GET("/member/{phone}")
+    Call<MemberInfoItem> selectMemberInfo(@Path("phone") String phone);
+
+    // 새로운 사용자 전화번호 등록
+    @FormUrlEncoded
+    @POST("/member/phone")
+    Call<String> insertMemberPhone(@Field("phone") String phone);
 }
